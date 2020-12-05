a4d1c11528051c51ac3c7b678bd66c26816fc1e3
liangming <liangming1@lightinthebox.com>
2020-12-06 00:55:15 +0800

    init hy-music-pc-react

---
+++ hy-music-pc-react/README.md
@@ -0,0 +1,9 @@
+### 目录结构
+- assets  静态资源
+- components 公共组件
+- common 公共数据、参数、常量
+- network 网络请求
+- pages  页面组件
+- router 路由配置 
+- store redux相关仓库 
+- utils 封装的工具相关
---
+++ hy-music-pc-react/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+import App from "./App";
+
+{/* <React.StrictMode> // 严格模式 */}
+
+ReactDOM.render(<App/>,document.getElementById("root"));
