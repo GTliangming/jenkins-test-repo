74da24396dade4300fbd725a7f8f918cf9732555
liangming <liangming1@lightinthebox.com>
2020-12-06 14:48:59 +0800

    顶部右侧部分

--- hy-music-pc-react/src/components/app-header/index.js
+++ hy-music-pc-react/src/components/app-header/index.js
@@ -1,55 +1,57 @@
-import React, { memo } from 'react'
+import React, { memo } from 'react';
+import { NavLink } from "react-router-dom";
+import classnames from "classnames";
 
-import { headerLinks } from "@/common/link-data"
+import { headerLinks } from "@/common/link-data";
 
-
+import { Input } from "antd";
+import { SearchOutlined } from '@ant-design/icons';
 
 import {
-    HeaderWrapper,
-    HeaderLeft,
-    HeaderRight
-} from "./style"
-import { NavLink } from 'react-router-dom'
-
+  AppHeaderWrapper,
+  HeaderLeft,
+  HeaderRight
+} from "./style";
 
 export default memo(function HYAppHeader() {
 
-
-
-    const showSelectItem = (item, index) => {
-        if (index < 3) {
-            return (
-                <NavLink to={item.link}>
-                    {item.title}
-                    <i className="sprite_01 icon" />
-                </NavLink>
-            )
-        } else {
-            return <a href={item.link}>{item.title}</a>
-        }
+  const showItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink to={item.link}>
+          {item.title}
+          <i className="sprite_01 icon"></i>
+        </NavLink>
+      )
+    } else {
+      return <a href={item.link} target="_blank" rel="noopener noreferrer">{item.title}</a>
     }
+  }
 
-
-    return (
-        <HeaderWrapper>
-            <div className="content wrap-v1">
-                <HeaderLeft>
-                    <a href="#/" className="logo sprite_01">网易云音乐</a>
-                    <div className="select-list">
-                        {
-                            headerLinks.map((item, index) => {
-                                return (
-                                    <div key={item.title} className="select-item">
-                                        {showSelectItem(item,index)}
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </HeaderLeft>
-                <HeaderRight>Right</HeaderRight>
-            </div>
-            <div className="divider"></div>
-        </HeaderWrapper>
-    )
+  return (
+    <AppHeaderWrapper>
+      <div className="wrap-v1 content">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="#/discover">网易云音乐</a>
+          <div className="select-list">
+            {
+              headerLinks.map((item, index) => {
+                return (
+                  <div className={classnames("select-item")} key={item.title}>
+                    {showItem(item, index)}
+                  </div>
+                )
+              })
+            }
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />} />
+          <div className="center">创作者中心</div>
+          <div className="">登录</div>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </AppHeaderWrapper>
+  )
 })
