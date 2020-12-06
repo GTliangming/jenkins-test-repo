50d2364aa0f4e94a12cfc781522a0985fe8c8a12
liangming <liangming1@lightinthebox.com>
2020-12-06 23:20:54 +0800

    一些打包配置

--- hy-music-pc-react/craco.config.js
+++ hy-music-pc-react/craco.config.js
@@ -2,6 +2,8 @@
 额外webpack配置
 
 */
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin')
+const webpack = require('webpack')
 const path = require('path');
 const resolve = dir => path.resolve(__dirname, dir);
 
@@ -10,6 +12,16 @@ module.exports = {
     alias: {
       '@': resolve("src"),
       'components': resolve("src/components")
-    }
+    },
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new SimpleProgressWebpackPlugin()
+    ],
+  },
+  babel: {
+    plugins: [
+      ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+      ['@babel/plugin-proposal-decorators', { legacy: true }]
+    ]
   }
 }
